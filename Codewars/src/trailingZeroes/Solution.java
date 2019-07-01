cf4e684a706558b1bb61c1ec1ee8f86d13a4a590
Amduras <sip90@web.de>
2019-07-01 10:20:03 +0200

    zeroes

--- Codewars/src/trailingZeroes/Solution.java
+++ Codewars/src/trailingZeroes/Solution.java
@@ -1,8 +1,16 @@
 package trailingZeroes;
 
 public class Solution {
-	  public static int zeros(int n) {
-	      // your beatiful code here
-	      return 0;
-	  }
+
+	public static void main(String[] args) {
+		System.out.println(zeros(6));
 	}
+	
+	public static int zeros(int n) {
+		int z = 0;
+		while((n/=5) > 0) {
+			z+=n;
+		}
+		return z;
+	}
+}
